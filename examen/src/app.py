c86aa2ef4fcb44eb95f4752522027e7e8ab4fbcd
dcasalv023 <plaracil3@gmail.com>
2024-03-07 10:02:43 +0100

    Añadidos

--- examen/src/app.py
+++ examen/src/app.py
@@ -1,20 +1,31 @@
-from flask import Flask, render_template
+# -*- coding: utf-8 -*-
+"""
+This module contains the Flask application.
+"""
+
 from datetime import datetime
 import locale
+from flask import Flask, render_template
 
 app = Flask(__name__)
 locale.setlocale(locale.LC_TIME, '')
 
 @app.route('/')
 def homepage():
-    # La variable x no se utiliza, así que la he eliminado
+    """
+    Render the homepage with current time and a random theme.
+    """
     the_time = datetime.now().strftime("%A, %d %b %Y %H:%M")
     return render_template("index.html", the_time=the_time, tema="dogs")
 
 @app.route('/status')
 def status():
+    """
+    Returns OK status.
+    """
     return "OK Todo"
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
+
 
--- examen/src/tests_flask_app.py
+++ examen/src/tests_flask_app.py
@@ -1,18 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains tests for the Flask application.
+"""
+
+# import pytest
 from app import app  # Flask instance of the API
 
 def test_index_route():
+    """
+    Test the index route.
+    """
     response = app.test_client().get('/status')
-
     assert response.status_code == 200
     assert response.data.decode('utf-8') == 'OK'
 
 def test_landing():
+    """
+    Test the landing page.
+    """
     landing = app.test_client().get('/')
     html = landing.data.decode()
-
     assert landing.status_code == 200
-
-    # Check text
-    assert "Daniel Castillejo" in html  # Agrega tu nombre y apellido aquí
+    assert "Daniel Castillejo Álvarez" in html
     assert "It is currently " in html
+
 
